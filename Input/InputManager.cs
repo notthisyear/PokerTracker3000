329141825b51d51eddaf46e6ba1a62072f7a1064
Calle Lindquist <lindquist.calle@gmail.com>
2023-06-09 00:44:02 +0200

    Refactor input logic so that it can be handled in the view directly

--- Input/InputManager.cs
+++ Input/InputManager.cs
@@ -1,16 +1,66 @@
-﻿using System;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 namespace PokerTracker3000.Input
 {
     public class InputManager
     {
-        public event EventHandler? StartButtonPressed;
-
-        public void HandleKeyPressed(KeyEventArgs e)
+        public record UserInputEvent
         {
-            if (e.Key == Key.Enter)
-                StartButtonPressed?.Invoke(this, EventArgs.Empty);
+            public enum NavigationDirection
+            {
+                None,
+                Left,
+                Right,
+                Up,
+                Down
+            }
+
+            public enum ButtonPressed
+            {
+                None,
+                Start,
+                Select,
+                Accept,
+                GoBack
+            }
+
+            public bool IsButtonPressed { get; init; } = false;
+
+            public bool IsNavigation { get; init; } = false;
+
+            public ButtonPressed Button { get; init; } = ButtonPressed.None;
+
+            public NavigationDirection Direction { get; init; } = NavigationDirection.None;
+
+            public bool IsEmpty => IsButtonPressed == false && IsNavigation == false;
+
+            public static UserInputEvent GetNavigationEvent(NavigationDirection direction)
+                => new()
+                {
+                    IsNavigation = true,
+                    Direction = direction
+                };
+
+            public static UserInputEvent GetStartButtonEvent()
+                => new()
+                {
+                    IsButtonPressed = true,
+                    Button = ButtonPressed.Start
+                };
+
+            public static UserInputEvent GetEmptyEvent()
+                => new();
         }
+
+        public static UserInputEvent GetUserInputEventFromKeyboard(KeyEventArgs e)
+            => e.Key switch
+            {
+                Key.Enter => UserInputEvent.GetStartButtonEvent(),
+                Key.Left => UserInputEvent.GetNavigationEvent(UserInputEvent.NavigationDirection.Left),
+                Key.Right => UserInputEvent.GetNavigationEvent(UserInputEvent.NavigationDirection.Right),
+                Key.Up => UserInputEvent.GetNavigationEvent(UserInputEvent.NavigationDirection.Up),
+                Key.Down => UserInputEvent.GetNavigationEvent(UserInputEvent.NavigationDirection.Down),
+                _ => UserInputEvent.GetEmptyEvent()
+            };
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using System.Windows.Media;
 using PokerTracker3000.Common;
 using PokerTracker3000.ViewModels;
 using PokerTracker3000.WpfComponents;
@@ -19,7 +24,29 @@ namespace PokerTracker3000
             typeof(MainWindow),
             new FrameworkPropertyMetadata(null));
 
+        public bool SideMenuOpen
+        {
+            get => (bool)GetValue(SideMenuOpenProperty);
+            set => SetValue(SideMenuOpenProperty, value);
+        }
+        public static readonly DependencyProperty SideMenuOpenProperty = DependencyProperty.Register(
+            nameof(SideMenuOpen),
+            typeof(bool),
+            typeof(MainWindow),
+            new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+        private enum CurrentFocusArea
+        {
+            None,
+            SideMenu,
+            Players,
+            BottomPanel
+        }
+
         private const string SettingsFileName = "Settings.json";
+        private CurrentFocusArea _lastFocusArea = CurrentFocusArea.None;
+        private CurrentFocusArea _currentFocusArea = CurrentFocusArea.None;
 
         public MainWindow()
         {
@@ -60,9 +87,85 @@ namespace PokerTracker3000
 
         private void KeyDownInWindow(object sender, KeyEventArgs e)
         {
-            if (ViewModel == default)
+            var inputEvent = Input.InputManager.GetUserInputEventFromKeyboard(e);
+
+            if (inputEvent.IsButtonPressed)
+                HandleButtonPressedEvent(inputEvent.Button);
+            else if (inputEvent.IsNavigation)
+                HandleNavigationEvent(inputEvent.Direction);
+        }
+
+        private void HandleButtonPressedEvent(Input.InputManager.UserInputEvent.ButtonPressed button)
+        {
+            if (button == Input.InputManager.UserInputEvent.ButtonPressed.Start)
+            {
+                SideMenuOpen = !SideMenuOpen;
+                if (SideMenuOpen)
+                {
+                    _lastFocusArea = _currentFocusArea;
+                    _currentFocusArea = CurrentFocusArea.SideMenu;
+                }
+                else
+                {
+                    _currentFocusArea = _lastFocusArea;
+                    if (_currentFocusArea != CurrentFocusArea.None)
+                        ShowFocusHighlightInArea(_currentFocusArea);
+                }
+            }
+            else
+            {
+                // TODO
+            }
+        }
+
+        private void HandleNavigationEvent(Input.InputManager.UserInputEvent.NavigationDirection direction)
+        {
+            var playerViews = GetPlayerViews();
+            if (_currentFocusArea == CurrentFocusArea.None)
+            {
+                _currentFocusArea = CurrentFocusArea.Players;
+                playerViews.First().IsHighlighted = true;
                 return;
-            ViewModel.InputManager.HandleKeyPressed(e);
+            }
+
+            if (_currentFocusArea == CurrentFocusArea.SideMenu)
+            {
+                // Handle navigation in side menu
+            }
+            else
+            {
+                if (direction == Input.InputManager.UserInputEvent.NavigationDirection.Up ||
+                    direction == Input.InputManager.UserInputEvent.NavigationDirection.Down)
+                {
+                    // TODO
+                }
+                else
+                {
+                    if (_currentFocusArea == CurrentFocusArea.Players)
+                    {
+
+                    }
+                }
+            }
+        }
+
+        private void ShowFocusHighlightInArea(CurrentFocusArea currentFocusArea) => throw new NotImplementedException();
+
+        private List<Player> GetPlayerViews()
+        {
+            List<Player> playerViews = new();
+            var container = ExtensionMethods.TryFindChild<UniformGrid>(playersOverview);
+
+            if (container == default)
+                return playerViews;
+
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(container);  i++)
+            {
+                var p = ExtensionMethods.TryFindChild<Player>(VisualTreeHelper.GetChild(container, i));
+                if (p != default)
+                    playerViews.Add(p);
+            }
+            return playerViews;
         }
     }
 }
