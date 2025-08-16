1c1d2674b8e33afcc1601af5906594f0cf0b4400
Calle Lindquist <lindquist.calle@gmail.com>
2025-08-16 12:35:58 +0200

    Fix layout bug and clashing animations

---
+++ WpfComponents/Converters/FocusAreaToBoolConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using static PokerTracker3000.MainWindowFocusManager;
+
+namespace PokerTracker3000.WpfComponents.Converters
+{
+    internal class FocusAreaToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter is not string s)
+                return false;
+            if (value is not FocusArea area)
+                return false;
+
+            foreach (var part in s.Split('|'))
+            {
+                if (Enum.TryParse<FocusArea>(part, out var a) && a == area)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
